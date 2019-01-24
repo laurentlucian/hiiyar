f25faed2464ce038f3e2b6051efa303ff82554e9
Pierre Ortega <pierreandreisortega@gmail.com>
2019-01-23 20:15:17 -0800

    responsive curve

--- src/modules/Hero.js
+++ src/modules/Hero.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { css } from "@emotion/core";
 
-import Header from "../vectors/header";
+import CurveSvg from "../vectors/curve";
 
 const Hero = () => {
   return (
-    <div>
-      <Header css={{ width: "100%" }} />
-    </div>
+    <section
+      css={css`
+        width: 100%;
+        height: 700px;
+        object-fit: contain;
+        background-image: linear-gradient(354deg, #8f1840, #741838 52%, #4d1226);
+        position: relative;
+      `}
+    >
+      <CurveSvg
+        css={css`
+          fill: white;
+          position: absolute;
+          bottom: 0;
+        `}
+      />
+    </section>
   );
 };
 
---
+++ src/vectors/curve.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default props => (
+  <svg
+    fillRule="evenodd"
+    clipRule="evenodd"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby="title"
+    viewBox="0 0 1920 240"
+    id="goop"
+    {...props}
+  >
+    <g>
+      <path d="M1920,144.5l0,95.5l-1920,0l0,-65.5c196,-36 452.146,-15.726 657.5,8.5c229.698,27.098 870,57 1262.5,-38.5Z" />
+    </g>
+  </svg>
+);
