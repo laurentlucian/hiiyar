3ffce7ff58962248b1616506a6fd78856cd2c5e8
Laurent Lucian <laurent.lucian@gmail.com>
2019-03-06 19:08:13 -0800

    some changes

--- src/modules/Contact.js
+++ src/modules/Contact.js
@@ -1,71 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import { css } from "@emotion/core";
 import { Heading3, Paragraph } from "../components/typography";
 import Content from "../components/content";
 import { FifthCurve as FifthCurveSvg } from "../vectors/curves";
 import RouterAnchor from "../components/routerAnchor";
 
-export default () => (
-  <section
-    css={css`
-      position: relative;
-    `}
-  >
-    <FifthCurveSvg
+const useInputValue = initialValue => {
+  const [text, setText] = useState(initialValue);
+  return {
+    text,
+    onChange: e => setText(e.target.value),
+  };
+};
+
+export default () => {
+  const name = useInputValue("");
+  const email = useInputValue("");
+  const description = useInputValue("");
+
+  return (
+    <section
       css={css`
-        position: absolute;
-        top: 0;
-        transform: translateY(-100%);
-        fill: white;
+        position: relative;
       `}
-    />
-    <RouterAnchor name="contact">
-      <Content
+    >
+      <FifthCurveSvg
         css={css`
-          display: flex;
-          height: 300px;
-          flex-direction: column;
-          align-items: center;
+          position: absolute;
+          top: 0;
+          transform: translateY(-100%);
+          fill: white;
         `}
-      >
-        <Heading3 red style={{ margin: 0 }}>
-          Ready when you are.
-        </Heading3>
-        <Paragraph heading center style={{ fontSize: "30px", color: "#4d4d4d" }}>
-          We're based in LA with a global network of influencers spanning in NA, SA and
-          SEA
-        </Paragraph>
-        <div>
-          <div
-            css={css`
-              display: flex;
-              justify-content: space-between;
-            `}
+      />
+      <RouterAnchor name="contact">
+        <Content
+          css={css`
+            display: flex;
+            height: 300px;
+            flex-direction: column;
+            align-items: center;
+          `}
+        >
+          <Heading3 red style={{ margin: 0 }}>
+            Ready when you are.
+          </Heading3>
+          <Paragraph heading center style={{ fontSize: "30px", color: "#4d4d4d" }}>
+            We're based in LA with a global network of influencers spanning in NA, SA and
+            SEA
+          </Paragraph>
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
           >
-            <div
+            <fieldset
               css={css`
-                display: flex;
-                flex-direction: column;
+                background-color: lightcoral;
+                text-decoration: none;
+                border: 0;
+
+                & input,
+                textarea {
+                  border-radius: 5px;
+                  border-width: 0;
+                  outline: none;
+                  width: 100%;
+                }
               `}
             >
-              <p>your name</p>
-              <input type="text" />
-            </div>
-            <div>
-              <p>your email</p>
-              <input type="email" />
-            </div>
-          </div>
-          <div>
-            <p>help you</p>
-            <p>character count</p>
-          </div>
-          <textarea name="describe" id="" cols="30" rows="10" />
-        </div>
-      </Content>
-    </RouterAnchor>
-  </section>
-);
+              <div
+                css={css`
+                  display: flex;
+                  justify-content: space-between;
+                  background-color: lightblue;
+                `}
+              >
+                <div>
+                  <p>Your name</p>
+                  <input name="text" maxLength="100" {...name} />
+                </div>
+                <div>
+                  <p>Your email</p>
+                  <input name="email" maxLength="100" {...email} />
+                </div>
+              </div>
+              <label
+                css={css`
+                  display: flex;
+                  justify-content: space-between;
+                `}
+              >
+                <p>Describe your needs</p>
+                <p>{255 - description.text.length} characters remaining</p>
+              </label>
+              <textarea
+                name="message"
+                cols="50"
+                rows="6"
+                maxLength="255"
+                {...description}
+              />
+            </fieldset>
+          </form>
+        </Content>
+      </RouterAnchor>
+    </section>
+  );
+};
 
 // <div
 //   css={css`
